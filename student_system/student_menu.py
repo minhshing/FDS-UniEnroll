aa3fb7f29d99194307630dd09314723488a8ab42
Rafeed Sultan <sultanrafeed@gmail.com>
2026-09-23 11:15:48 +1000

    feat: fixes at willy and minh's code

--- student_system/student_menu.py
+++ student_system/student_menu.py
@@ -1,6 +1,11 @@
 from student_system.student_controller import StudentController
 
+# Rafeed's code - Subject Enrolment System
+from student_system.enrolment_controller import EnrolmentController
+from student_system.enrolment_menu import EnrolmentMenu
+
 from utils.constants import StudentMenuInputConstants
+
 
 class StudentMenu:
     def __init__(self, controller: StudentController):
@@ -19,3 +24,13 @@ class StudentMenu:
                 break
             else:
                 print("Invalid input")
+
+
+# Rafeeds code- enrolment menu system
+    def open_enrolment_menu(self, student):
+
+        enrolment_controller = EnrolmentController(
+            student, self.controller.database
+        )
+        enrolment_menu = EnrolmentMenu(enrolment_controller)
+        enrolment_menu.run()
